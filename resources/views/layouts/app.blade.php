396b2228556b12020d591f0bb824606dfbb0391d
Chris Roberts <chris@829llc.com>
2019-10-03 08:30:19 -0400

    Updated main structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,22 +4,12 @@
 
 <body @php body_class() @endphp>
   @php do_action('get_header') @endphp
-  @include('partials.header')
   <div class="wrap container" role="document">
-    @include('vue.hello-world')
-    <div class="content">
-      <main class="main">
-        @yield('content')
-      </main>
-      @if (App\display_sidebar())
-      <aside class="sidebar">
-        @include('partials.sidebar')
-      </aside>
-      @endif
+    <div id="app">
+      <hello-world message="Hello world!"></hello-world>
     </div>
   </div>
   @php do_action('get_footer') @endphp
-  @include('partials.footer')
   @php wp_footer() @endphp
 </body>
 
