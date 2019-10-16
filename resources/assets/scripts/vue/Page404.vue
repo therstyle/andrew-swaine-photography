a06ff31bcba126b4b3966d4d645cdaf4bc98d61f
Chris Roberts <chris@829llc.com>
2019-10-16 08:29:38 -0400

    Added basic structure for 404 page

--- resources/assets/scripts/vue/Page404.vue
+++ resources/assets/scripts/vue/Page404.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h1>404 Error</h1>
-  </div>
+  <article>
+    <h1 class="page-title">404 Error</h1>
+  </article>
 </template>
 
 <script>
@@ -9,6 +9,21 @@ export default {
   name: 'page-404',
   data: function() {
     return {
+      pageTitle: '',
+      pageText: '',
+      restUrl: `${wp.url}/wp-json/as/v1/global`
+    }
+  },
+  created: function() {
+    this.loadData(this.restUrl);
+  },
+  methods: {
+    loadData(url) {
+      fetch(url).
+      then(response => response.json()).
+      then(data => {
+        console.log(data);
+      })
     }
   }
 }
