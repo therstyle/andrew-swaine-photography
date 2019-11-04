34deb2ff521e765cb53a1769529b1e65ddcbfe94
Chris Roberts <chris@829llc.com>
2019-11-04 14:29:51 -0500

    Added count for current gallery

--- resources/assets/scripts/vue/Page.vue
+++ resources/assets/scripts/vue/Page.vue
@@ -9,17 +9,19 @@
       </ul>
     </nav>
     
-    <div v-if="gallery" class="gallery">
-      <div v-for="(galleryItem, index) in gallery" :key="index" class="gallery-item">
-        <img :src="galleryItem.url" :alt="galleryItem.alt">
+    <div v-if="gallery" id="gallery">
+      <div class="gallery-photos">
+        <div v-for="(galleryItem, index) in gallery" :key="index" class="gallery-item">
+          <img :src="galleryItem.url" :alt="galleryItem.alt">
+        </div>
+      </div>
+
+      <div class="gallery-details">
+        <h1 class="page-title">{{ pageTitle }} <span class="count">{{ current }} / {{ total }}</span></h1>
+        <!-- controls -->
       </div>
     </div>
 
-    <!-- add gallery controls here -->
-    <div v-if="gallery">
-      <!-- heading -->
-      <!-- controls -->
-    </div>
   </article>
 </template>
 
@@ -30,7 +32,9 @@ export default {
     return {
       pageTitle: '',
       menu: [],
-      gallery: []
+      gallery: [],
+      current: 1,
+      total: 0
     }
   },
   props: {
@@ -59,6 +63,7 @@ export default {
         this.pageTitle = data.page_title;
         this.menu = data.menu;
         this.gallery = data.gallery;
+        this.total = data.gallery.length
       })
     }
   }
