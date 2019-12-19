6803491b3baaf5845d27a6e11822d74d2afdb263
Chris <therstyle@gmail.com>
2019-12-19 14:33:51 -0500

    Updated WP router function

--- resources/assets/scripts/vue/vueRoutes.js
+++ resources/assets/scripts/vue/vueRoutes.js
@@ -1,12 +1,20 @@
 import Home from './Home.vue';
 import Page from './Page.vue';
 
-export default [
-  {path: '/', component: Home, props: {pageId: 6}},
-  {path: '/people-photography/', component: Page, props: {pageId: 15}},
-  {path: '/people-photography/fashion/', component: Page, props: {pageId: 29}},
-  {path: '/contact', component: Page, props: {pageId: 72}},
-  {path: '*', component: Page, props: {pageType: '404'}},
-]
+const vueRoutes = wp['routes'].map(route => ({path: route.path, component: route.component, props: route.props}));
+
+console.log(vueRoutes);
+
+// export default [
+//   {path: '/', component: Home, props: {pageId: 6}},
+//   {path: '/people-photography/', component: Page, props: {pageId: 15}},
+//   {path: '/people-photography/fashion/', component: Page, props: {pageId: 29}},
+//   {path: '/contact', component: Page, props: {pageId: 72}},
+//   {path: '*', component: Page, props: {pageType: '404'}},
+// ]
 
 //export default wp.routes;
+
+
+
+export default vueRoutes;
