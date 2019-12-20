edbbb6deac7e82846c9dfc49aaf2580225c6be98
Chris <therstyle@gmail.com>
2019-12-19 20:20:08 -0500

    Updated wp router function

--- resources/assets/scripts/vue/vueRoutes.js
+++ resources/assets/scripts/vue/vueRoutes.js
@@ -5,10 +5,16 @@ import Vue from 'vue';
 Vue.component('Home', Home);
 Vue.component('Page', Page);
 
-const vueRoutes = wp['routes'].map(route => ({
-  path: route.path, 
-  component: Vue.component(route.component), 
-  props: {pageId: parseInt(route.props.pageId)}}));
+const vueRoutes = wp['routes'].map(route => {
+  let id = route.props.pageId ? parseInt(route.props.pageId) : null;
+  let type = route.props.pageType ? route.props.pageType : null;
+
+  return {
+    path: route.path,
+    component: Vue.component(route.component),
+    props: { pageId: id, pageType: type }
+  }
+});
 
 console.log(vueRoutes);
 
